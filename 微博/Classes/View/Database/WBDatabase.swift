32ca0038812a900607d12938f04bdf5384f1dd9c
codeDjb <ivt502@gaojiangshangaojiangyingdeMacBook-Pro.local>
2017-03-17 15:43:24 +0800

    导入FMDB框架

---
+++ 微博/Classes/View/Database/WBDatabase.swift
@@ -0,0 +1,13 @@
+//
+//  WBDatabase.swift
+//  微博
+//
+//  Created by IVT502 on 2017/3/17.
+//  Copyright © 2017年 IVT. All rights reserved.
+//
+
+import UIKit
+
+class WBDatabase: NSObject {
+
+}
--- 微博/Weibo-Bridge.h
+++ 微博/Weibo-Bridge.h
@@ -8,6 +8,7 @@
 
 #import "JPUSHService.h"
 #import "MJRefresh.h"
+#import "FMDB.h"
 // iOS10注册APNs所需头文件
 #ifdef NSFoundationVersionNumber_iOS_9_x_Max
 #import <UserNotifications/UserNotifications.h>
